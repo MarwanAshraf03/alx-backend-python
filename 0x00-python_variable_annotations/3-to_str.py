364b683a1d35d99bd9075611917faee6fbf46aad
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-13 17:44:05 +0300

    3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""to string module"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: returns the string representation of a number
+    n: number to be stringified
+    """
+    return str(n)
