78e94df6bf2aa3c8d01f54d882c9568f81b67408
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-13 17:39:35 +0300

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: returns contatination of two strings
+    str1: first string
+    str2: second string
+    """
+    return f"{str1}{str2}"
