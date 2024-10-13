e0ff3b4298cf0b2cd18d1c87aa493635b10bc8e8
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-13 17:36:46 +0300

    0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Adding module"""
+
+
 def add(a: float, b: float) -> float:
-	"""
-	add: function adding two number
-	a: first number
-	b: second number
-	"""
-	return (a + b)
+    """
+    add: function adding two number
+    a: first number
+    b: second number
+    """
+    return (a + b)
