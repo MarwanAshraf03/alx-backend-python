e75500ce14d7045ac83a93cd498aa577bdc9cae0
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-14 01:36:26 +0300

    102

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """same first element module"""
-import typing
+# import typing
+from typing import Union, Any, Sequence
 import types
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing[typing.Any | types.NoneType]:
+# def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Union[typing.Any | types.NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[types.NoneType, Any]:
     if lst:
         return lst[0]
     else:
