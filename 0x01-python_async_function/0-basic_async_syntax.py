03f86cfd6821a7cabe933ab4122590ce76136d43
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-15 02:13:02 +0300

    0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """waits for a random period between 0 and max_delay"""
     wait: float = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
