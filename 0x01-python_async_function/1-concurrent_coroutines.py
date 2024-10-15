bbce5fcb1e85d45914df5553b0e87423e3eb512d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-15 03:26:34 +0300

    1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> list[float]:
     """waits for a random period between 0 and max_delay"""
-    retlst: list[float] = []
+    tasks = []
     for _ in range(n):
-        retlst.append(await wait_random(max_delay))
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    retlst: list[float] = [await task for task in asyncio.as_completed(tasks)]
     return retlst
