42aa821e9865e2e8f3d83e032282756709839e03
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-19 02:32:00 +0300

    1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module"""
+import typing
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """
+    uses async comprehension to collect 10 numbers
+    from async_generator function
+    """
+    ll: typing.List[float]= []
+    async for i in async_generator():
+        ll.append(i)
+    return (ll)
