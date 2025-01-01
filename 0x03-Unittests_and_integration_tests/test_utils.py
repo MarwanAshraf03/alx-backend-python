084f33bd8cb8140e1cc01f2f2fe34de62ed56156
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-01 17:38:48 +0200

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
                                expected: Any) -> None:
+        """test access nested map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
