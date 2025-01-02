857f15d73198b3c47331281049167c25c3b500f5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-02 14:09:36 +0200

    5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module"""
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import PropertyMock, patch, Mock
 import unittest.mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Any
@@ -10,6 +10,7 @@ from utils import memoize
 from client import GithubOrgClient
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """a clss for testing client.py"""
@@ -26,3 +27,13 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(GithubOrgClient(org).org, ret)
         self.assertEqual(MK1.call_count, 1)
+
+    @parameterized.expand([
+        ("google", "great"),
+        ("abc", "great"),
+    ])
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, url: str, ret: str, MK_ret: Mock):
+        """"""
+        MK_ret.return_value = {"repos_url": "great"}
+        self.assertEqual(GithubOrgClient(url)._public_repos_url, ret)
